1e8dc02ddbe145c4be9363dbe3ae56b0895488e6
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2023-11-21 16:54:01 +0530

    feat: added react setup

---
+++ craco.config.ts
@@ -0,0 +1,17 @@
+import path from "path";
+
+const cracoConfig = {
+  webpack: {
+    alias: {
+      "@/public": path.resolve(__dirname, "./public"),
+      "@/src": path.resolve(__dirname, "./src"),
+      "@/pages": path.resolve(__dirname, "./src/pages"),
+      "@/styles": path.resolve(__dirname, "./src/styles"),
+      "@/components": path.resolve(__dirname, "./src/components"),
+      "@/hooks": path.resolve(__dirname, "./src/hooks"),
+      "@/utils": path.resolve(__dirname, "./src/utils"),
+    },
+  },
+};
+
+module.exports = cracoConfig;
